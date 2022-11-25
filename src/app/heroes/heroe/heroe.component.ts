c3f42d79ae79ca8e3b2773bd99fa42873763f5a2
Mario Toj <mariotojalvarez@gmail.com>
2022-11-25 13:59:18 -0600

    primer commit

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,42 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+
+    selector: 'app-heroe',
+    templateUrl: 'heroe.component.html'
+})
+export class HeroeComponent {
+ 
+    
+    nombre: string = 'Iron Man'
+    edad: number = 45
+
+    get nombreCapitalizado(): string {
+
+        return this.nombre.toUpperCase()
+    }
+
+    obetenerNombre():string {
+
+        return ` ${this.nombre} - ${this.edad} `
+    }
+
+    cambiarNombre():void {
+
+        this.nombre = 'Spiderman'
+    }
+
+    cambiarEdad():void {
+
+        this.edad = 27
+    }
+
+    cambiarNombreEdad():void {
+
+        this.nombre = 'Spiderman'
+        this.edad = 27
+
+    }
+}
